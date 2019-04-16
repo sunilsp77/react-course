17657c9abee45a5892fd52505b96f004a7e86aad
Sunil Prakash <sunil.thind17@gmail.com>
2019-04-16 20:52:09 +0530

    Using React hooks to manage state in functional components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,69 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-  state = {
+// class App extends Component {
+//   state = {
+//     persons: [
+//       { name: 'Max', age: 28 },
+//       { name: 'Manu', age: 29 },
+//       { name: 'Stephanie', age: 26 },
+//     ],
+//   };
+
+//   switchNameHandler = () => {
+//     // console.log('Was clicked');
+//     // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
+//     this.setState({
+//       persons: [
+//         { name: 'Maximilian', age: 28 },
+//         { name: 'Manu', age: 29 },
+//         { name: 'Stephanie', age: 27 },
+//       ],
+//     });
+//   };
+
+//   render() {
+//     return (
+//       <div className="App">
+//         <h1>Hello I m REACT</h1>
+//         <button onClick={this.switchNameHandler}>Switch Name</button>
+//         <Person
+//           name={this.state.persons[0].name}
+//           age={this.state.persons[0].age}
+//         />
+//         <Person
+//           name={this.state.persons[1].name}
+//           age={this.state.persons[1].age}
+//         >
+//           My Hobbies: Racing
+//         </Person>
+//         <Person
+//           name={this.state.persons[2].name}
+//           age={this.state.persons[2].age}
+//         />
+//       </div>
+//     );
+//   }
+// }
+
+const app = props => {
+  const [personsState, setPersonsState] = useState({
     persons: [
       { name: 'Max', age: 28 },
       { name: 'Manu', age: 29 },
       { name: 'Stephanie', age: 26 },
     ],
-  };
+  });
 
-  switchNameHandler = () => {
+  const [otherState, setOtherState] = useState('some other value');
+
+  console.log(personsState, otherState);
+
+  const switchNameHandler = () => {
     // console.log('Was clicked');
     // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
-    this.setState({
+    setPersonsState({
       persons: [
         { name: 'Maximilian', age: 28 },
         { name: 'Manu', age: 29 },
@@ -23,28 +72,26 @@ class App extends Component {
     });
   };
 
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello I m REACT</h1>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age}
-        />
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-        >
-          My Hobbies: Racing
-        </Person>
-        <Person
-          name={this.state.persons[2].name}
-          age={this.state.persons[2].age}
-        />
-      </div>
-    );
-  }
-}
+  return (
+    <div className="App">
+      <h1>Hello I m REACT</h1>
+      <button onClick={switchNameHandler}>Switch Name</button>
+      <Person
+        name={personsState.persons[0].name}
+        age={personsState.persons[0].age}
+      />
+      <Person
+        name={personsState.persons[1].name}
+        age={personsState.persons[1].age}
+      >
+        My Hobbies: Racing
+      </Person>
+      <Person
+        name={personsState.persons[2].name}
+        age={personsState.persons[2].age}
+      />
+    </div>
+  );
+};
 
-export default App;
+export default app;
