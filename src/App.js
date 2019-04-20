20a6069fd9edb2d0f3abca76d3a4852299194714
Sunil Prakash <sunil.thind17@gmail.com>
2019-04-20 13:22:19 +0530

    Modified the nameChangedHandler method to work with id. in it updated the state without mutating the state directly by creating copies.

--- src/App.js
+++ src/App.js
@@ -13,13 +13,23 @@ class App extends Component {
     showPersons: false,
   };
 
-  nameChangedHandler = event => {
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = { ...this.state.persons[personIndex] };
+
+    //The below statement is an alternative for the above statement
+    // const person = Object.assign({}, this.state.persons[personIndex]);
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
     this.setState({
-      persons: [
-        { name: 'Max', age: 28 },
-        { name: event.target.value, age: 29 },
-        { name: 'Stephanie', age: 26 },
-      ],
+      persons: persons,
     });
   };
 
@@ -56,6 +66,7 @@ class App extends Component {
                 name={person.name}
                 age={person.age}
                 key={person.id}
+                changed={event => this.nameChangedHandler(event, person.id)}
               />
             );
           })}
