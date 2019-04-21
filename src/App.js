053ffae5d1823f2fdafd04364fada2742266ea24
Sunil Prakash <sunil.thind17@gmail.com>
2019-04-21 10:25:36 +0530

    Used Radium to use media queries in Person component. Wrapped entire application in StyleRoot to handle warning

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
-import Radium from 'radium';
+import Radium, { StyleRoot } from 'radium';
 
 class App extends Component {
   state = {
@@ -95,14 +95,16 @@ class App extends Component {
     }
 
     return (
-      <div className="App">
-        <h1>Hello I'm React App</h1>
-        <p className={classes.join(' ')}>This is really working!</p>
-        <button style={style} onClick={this.togglePersonsHandler}>
-          Toggle Persons
-        </button>
-        {persons}
-      </div>
+      <StyleRoot>
+        <div className="App">
+          <h1>Hello I'm React App</h1>
+          <p className={classes.join(' ')}>This is really working!</p>
+          <button style={style} onClick={this.togglePersonsHandler}>
+            Toggle Persons
+          </button>
+          {persons}
+        </div>
+      </StyleRoot>
     );
   }
 }
