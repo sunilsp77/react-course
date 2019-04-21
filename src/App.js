86c37623685a050e98e655e7905ce6b7e7d815ad
Sunil Prakash <sunil.thind17@gmail.com>
2019-04-21 10:04:44 +0530

    Added and used Radium for working on inline styles with Pseudo selectors without changing the scope of inline styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
+import Radium from 'radium';
 
 class App extends Component {
   state = {
@@ -54,6 +55,10 @@ class App extends Component {
       border: '1px solid blue',
       padding: '8px',
       cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black',
+      },
     };
 
     let persons = null;
@@ -75,6 +80,10 @@ class App extends Component {
       );
 
       style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black',
+      };
     }
 
     const classes = [];
@@ -145,4 +154,4 @@ class App extends Component {
 //   );
 // };
 
-export default App;
+export default Radium(App);
