4eaa978b89d551934fa8a70a87a2606ab2605beb
Sunil Prakash <sunil.thind17@gmail.com>
2019-04-23 18:23:41 +0530

    Component Creation in Lifecycle in Action

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -22,7 +22,7 @@ const cockpit = props => {
   }
   return (
     <div>
-      <h1>Hello I'm React App</h1>
+      <h1>{props.title}</h1>
       <p className={classes.join(' ')}>This is really working!</p>
       <button style={style} onClick={props.clicked}>
         Toggle Persons
--- src/containers/App.js
+++ src/containers/App.js
@@ -4,6 +4,11 @@ import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    console.log('[App.js] constructor');
+  }
+
   state = {
     persons: [
       { id: '1', name: 'Max', age: 28 },
@@ -13,6 +18,15 @@ class App extends Component {
     otherState: 'some other value',
     showPersons: false,
   };
+
+  static getDerivedStateFromProps(props, state) {
+    console.log('[App.js] getDerivedStateFromProps', props);
+    return state;
+  }
+
+  componentDidMount() {
+    console.log('[App.js] componentDidMount');
+  }
 
   nameChangedHandler = (event, id) => {
     const personIndex = this.state.persons.findIndex(p => {
@@ -48,6 +62,7 @@ class App extends Component {
   };
 
   render() {
+    console.log('[App.js] render');
     let persons = null;
     if (this.state.showPersons) {
       persons = (
@@ -62,6 +77,7 @@ class App extends Component {
     return (
       <div className="App">
         <Cockpit
+          title={this.props.appTitle}
           showPersons={this.state.showPersons}
           persons={this.state.persons}
           clicked={this.togglePersonsHandler}
